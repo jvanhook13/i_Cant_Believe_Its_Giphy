1aae6868479740a1b867b4cb3a2058ffe1b056f0
jvanhook13 <jaredvanhook1313@gmail.com>
2020-02-29 02:47:54 -0800

    commented out my javascript so far working on gettin gigfs to display

--- assets/javascript/stuff.js
+++ assets/javascript/stuff.js
@@ -1,49 +1,79 @@
 var topics = ["memes" , "subarus" , "bunnies" , "crackheads" , "narwhals" , "jif"]
-//  var apiURL = "https://api.giphy.com/v1/gifs/search?api_key=Txl5zxTtp7uNcJisLz536WRVX8Op4ZX7&q=" + gif + "&limit=10&offset=3&rating=PG-13&lang=en"
- function something() {
 
-//      $.ajax({
-//          url: apiURL ,
-//          method: "GET"
+ function displayGifs() {
+    var apiURL = "http://api.giphy.com/v1/gifs/search?q=" + gif + "s&api_key=dc6zaTOxFJmzC&limit=10";
+    var gif = $(this).attr("data-name")
 
-//     }).then(function(response) {
-//         var newDiv = $("#<div>")
-//         var gifRating = response.rating
-//         var newGifs = response.url
+     $.ajax({
+         url: apiURL ,
+         method: "GET"
+
+    }).then(function(response) {
+        console.log(response , "response")
+        //new div to hold gifs and ratings
+        var newDiv = $("#<div>")
+        // grab rating from ajax request
+        var gifRating = response.rating
+        console.log(response.rating)
+        // grab gif url from ajax request
+        var newGifs = response.url
+        // creates new paragraph tag for ratings
+        var newP = $("<p>")
+        // puts gif rating in new p
+        newP.text(gifRating)
+        //appends the new partagraph into the new div
+        newDiv.append(newP)
+        //appends newDiv onto the gifs-view div
+        newDiv.appendTo("#gifs-view")
+        // creates a new img tag for the gif url
+        var newGif = $("<img>")
+        // puts the img source as the gif url 
+        newGif.attr("src" , newGifs)
+        //appends newGif to newDiv
+        newDiv.append(newGif)
+        // appends newDiv onto gifs-view
+        newDiv.appendTo("#gifs-view")
         
-//         console.log(newGifs)
+        console.log(newGifs)
 
 
-//  })
+ })
 
 }
 
 function makeButtons() {
-
+//emptys previous buttons so we dont get dupes
 $("#buttons-view").empty() ;
 
+//loops throughy topics
 for (var i = 0; i < topics.length; i++) {
-    var b = $("<button>");
-    b.addClass("gif");
-    b.attr("data-name", topics[i]) ;
-    b.text(topics[i]) ;
-    $("#buttons-view").append(b) ;
+    //makes new button
+    var button = $("<button>");
+    // gives button class "gif"
+    button.addClass("gif");
+    //gives the button data-name
+    button.attr("data-name", topics[i]) ;
+    //fills in the text for the button
+    button.text(topics[i]) ;
+    //appends button into buttons view
+    $("#buttons-view").append(button) ;
 
 
 } }
 
 $("#add-gif").on("click", function(event) {
     event.preventDefault();
-
+    // allows the form to get a value
     var gif = $("#gif-input").val().trim();
-
+    // pushes that value into the topics array 
     topics.push(gif);
 
-
+    //runs make buttons on click
     makeButtons(); 
 
 });
 
-$(document).on("click" , ".gif" , something)
-
+// event handler for clicks on buttons with class gif?
+$(document).on("click" , ".gif" , displayGifs)
+// runs make buttons
 makeButtons() ;
